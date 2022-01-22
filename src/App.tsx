f0fc69a220fcdb920f9da1260f9fbeb4996d513c
taeheon <dalo952003@gmail.com>
2022-01-22 20:33:50 +0900

    기초 커밋 및 h1~h6 구현 완료 bold, italic, 취소선 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import EditorDiv from "./EditorDiv";
 
 function App() {
+  // const ref = useRef<HTMLIFrameElement | null>(null);
+
+  // React.useEffect(() => {
+  //   ref.current
+  //     ? ref.current.contentDocument
+  //       ? (ref.current.contentDocument.designMode = "on")
+  //       : console.log()
+  //     : console.log();
+  // }, []);
+
+  // const onClickButton = useCallback(() => {
+  //   // ref.current.sele
+  //   ref.current
+  //     ? ref.current.contentDocument
+  //       ? ref.current.contentDocument.execCommand("bold", false)
+  //       : console.log()
+  //     : console.log();
+  // }, []);
+
+  const onClick: React.MouseEventHandler = (e) => {
+    console.log(e.target);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <EditorDiv />
+    </>
   );
 }
 
---
+++ src/EditorDiv.tsx
@@ -0,0 +1,85 @@
+import React, {
+  useRef,
+  useCallback,
+  useState,
+  useEffect,
+  useMemo,
+} from "react";
+import { AiOutlineBold } from "react-icons/ai";
+import styled from "styled-components";
+
+const EditorBox = styled.div`
+  display: flex;
+  height: 1000px;
+`;
+
+const Editor = styled.textarea`
+  width: 50%;
+  resize: none;
+  padding: 10px;
+  height: 100%;
+`;
+
+const EditorViewer = styled.div`
+  padding: 10px;
+  width: 50%;
+  height: 100%;
+`;
+
+function EditorDiv() {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [text, setText] = useState<string>("");
+  const handleText = useCallback<React.ChangeEventHandler<HTMLTextAreaElement>>(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setText(e.target?.value);
+    },
+    []
+  );
+
+  const innerHtmlText = useMemo<string>(() => {
+    const splitedText = text.replace(/\n\n/g, "<br/>\n").split("\n");
+    console.log(splitedText);
+    const viewText = splitedText.map((text) => {
+      return (
+        text
+          // header
+          .replace(
+            /^(#{1,6}) (.+)/g,
+            (str, p1, p2) => `<h${p1.length}>${p2}</h${p1.length}>`
+          )
+          // bold
+          .replace(/\*\*(.+)\*\*/g, "<span style='font-weight:bold'>$1</span>")
+          // Italic
+          .replace(/\*(.+)\*/g, "<span style='font-style:italic'>$1</span>")
+          .replace(/_(.+)_/g, "<span style='font-style:italic'>$1</span>")
+          // 취소선
+          .replace(
+            /~~(.+)~~/g,
+            "<span style='text-decoration:line-through'>$1</span>"
+          )
+          .replace(/\n\n/g, "</br>")
+      );
+    });
+    return viewText.join("");
+  }, [text]);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.innerHTML = innerHtmlText;
+    }
+  }, [innerHtmlText]);
+
+  return (
+    <>
+      <div>
+        <AiOutlineBold />
+      </div>
+      <EditorBox>
+        <Editor onChange={handleText} />
+        <EditorViewer ref={ref} />
+      </EditorBox>
+    </>
+  );
+}
+
+export default EditorDiv;
